2b9254b6b457c5575c79bf874cb8ced90038d293
Bertalan Körmendy <korber314@gmail.com>
2018-01-06 13:13:22 +0100

    added basic 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default class NotFound extends React.Component {
+	render(){
+		return(
+			<div style={{margin: 'auto', color: 'white'}}>
+				Page not found
+			</div>
+			);
+	}
+}
