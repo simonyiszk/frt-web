5b1b472e7f02ed847d87d2d8cd350deb3562a524
Bertalan Körmendy <korber314@gmail.com>
2018-01-16 22:01:28 +0100

    css & image updates

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,38 +4,41 @@ import Media from "react-media";
 import FacebookLogo from "./facebook.svg";
 import InstaLogo from "./instagram.svg";
 import MailLogo from "./mail.svg";
+import YouTube from "./youtube.svg"
 
-import FRTLogo from "./frt_logo.png";
+import FRTLogo from "./frt_logo.svg";
 
 import Render from "./BME_FRT_Render_Side.png";
 
 export default class IndexPage extends React.Component {
 	render(){
 		return(
-			<div style={{display: "flex", flexDirection: 'column', position: 'absolute', left: 0, right: 0, bottom: 0, top: 0}}>
-				<img src={Render} style={{width: 'auto', height: 'auto', position: 'absolute', left: 0}} />
-
-				<div style={{display: "flex", justifyContent: 'center', alignItems: 'center', verticalAlign: 'middle', height: '20%'}}>
-					<img src={FRTLogo} style={{width: 133, height: 133, margin: 20}} />
-					<h3 style={{color: 'white', alignItems: 'center'}}>BME Formula Racing Team</h3>
+			<div style={{display: "flex", flexDirection: 'column', position: 'absolute', left: 0, right: 0, bottom: 0, top: 0, backgroundImage: "url("+Render+"), linear-gradient(to right, #242424 , #C8102E)", backgroundRepeat: 'no-repeat'}}>
+				<div style={{display: "flex", justifyContent: 'center',  verticalAlign: 'middle', height: '15%', margin: '20'}}>
+					<img src={FRTLogo} style={{margin: '20'}}/>
 				</div>
 
-				<div style={{display: 'flex', flexDirection: 'column', textAlign: 'center', height: '60%'}}>
-					<h1 style={{fontSize: 55}}>Az oldal fejlesztés alatt áll.</h1>
-					<h1  style={{fontSize: 55}}>Hamarosan visszatérünk.</h1>
+				<div style={{display: 'flex', flexDirection: 'column', verticalAlign: 'middle', textAlign: 'center', height: '70%', margin: 'auto'}}>
+						<h1 style={{fontSize: '3rem', zIndex: 10}}>Az oldal fejlesztés alatt áll.</h1>
+						<h1  style={{fontSize: '3rem', zIndex: 10}}>Hamarosan újra itt vagyunk.</h1>
 				</div>
 
-				<div style={{display: "flex", justifyContent: 'center', height: '20%'}}>
+				<div style={{display: "flex", justifyContent: 'center', height: '15%', zIndex: 10, margin: 20}}>
+
 					<a href="https://www.facebook.com/FormulaRacingTeam/">
-						<img src={FacebookLogo} style={{margin: 50, flex: 1, height: 50}}/>
+						<img src={FacebookLogo} style={{margin: '20', flex: 1, height: 50}}/>
 					</a>
 	
 					<a href="https://www.instagram.com/bme_formularacingteam/">
-						<img src={InstaLogo} style={{margin: 50, flex: 1, height: 50}}/>
+						<img src={InstaLogo} style={{margin: '20', flex: 1, height: 50}}/>
 					</a>
 	
 					<a href="mailto:info.bme.frt@gmail.com">
-						<img src={MailLogo} style={{margin: 50, flex: 1, height: 50}}/>
+						<img src={MailLogo} style={{margin: '20', flex: 1, height: 50}}/>
+					</a>
+
+					<a href="https://www.youtube.com/user/bmeFRT">
+						<img src={YouTube} style={{margin: '20', flex: 1, height: 50}}/>
 					</a>
 				</div>
 			</div>
