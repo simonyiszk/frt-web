6f4c19f799a7b6986ee0e227dd53f05d15562558
Bertalan Körmendy <korber314@gmail.com>
2018-02-18 18:49:39 +0100

    removed waypoint

--- src/pages/cars.jsx
+++ src/pages/cars.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import Waypoint from "react-waypoint";
 
 import Helmet from "react-helmet";
 import PostListing from "../components/PostListing";
@@ -33,6 +32,7 @@ const Ul = styled.ul`
   li {
     margin: 5px;
     display: flex;
+    cursor: hand;
   }
 `
 
