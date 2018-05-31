7275bd053672f24b7c124cf40abcc4b47bdc54c6
Bertalan Körmendy <korber314@gmail.com>
2018-05-31 09:54:57 +0200

    pontosítások

--- TODO.js
+++ TODO.js
@@ -1,25 +1,24 @@
 const todos =
     [
         "oldal-integráció qraphql-el",
-        "többnyelvűség",
-        "meghívások a cms-be",
+        "többnyelvűség implementálása",
+        "meghívások a gatsby cms-be",
         "tesztelés",
-        "kis css update-ek",
-        "további teendők vázolása a körben",
-        "reszponzivitás",
-        'paypal integration',
-        "why support us",
+        "css update-ek figmának megfelelően",
+        "reszponzivitás implementálása",
+        "paypal integráció az adományokhoz",
+        "why support us szöveg elhelyezése",
     ]
 
-    const design = [
-        "page navigation",
-        "szponzorok - több látsszon egy képernyőn",
-        "logó - piros aláhúzás nem kell",
-        "betűtípus csere",
-    ]
+const design = [
+    "page navigation (figmának megfelelően)",
+    "szponzorok - több látsszon egy képernyőn (sponsors screen)",
+    "logó - piros aláhúzás nem kell",
+    "betűtípus csere figmának megfelelően",
+]
 
-    const temp = [
-        "adó egy százaléka"
-    ]
+const temp = [
+    "adó egy százaléka szöveg elhelyezése"
+]
 
 export default todos;
