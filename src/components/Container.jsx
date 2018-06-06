8c1b93e0251885ac8b6573b3131e3de656fce117
Kristóf Poduszló <kripod@protonmail.com>
2018-06-06 22:46:47 +0200

    Add support for fluid containers

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,18 +2,23 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './Container.module.scss';
 
-const Container = ({ children, className, ...props }) => (
-  <div className={`${styles.root} ${className}`} {...props}>
+const Container = ({ children, fluid, className, ...props }) => (
+  <div
+    className={`${styles.root} ${!fluid && styles.nonFluid} ${className}`}
+    {...props}
+  >
     {children}
   </div>
 );
 
 Container.propTypes = {
   children: PropTypes.node.isRequired,
+  fluid: PropTypes.bool,
   className: PropTypes.string,
 };
 
 Container.defaultProps = {
+  fluid: false,
   className: '',
 };
 
