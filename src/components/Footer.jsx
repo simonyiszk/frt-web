35e78cc2aee3cd3e25597964e219d05b30ff3606
Kristóf Poduszló <kripod@protonmail.com>
2018-06-09 22:11:26 +0200

    Improve the appearance of footer buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,43 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faFacebookF,
   faInstagram,
   faYoutube,
 } from '@fortawesome/free-brands-svg-icons';
-import { faCircle, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 import React from 'react';
+import CircledFontAwesomeIcon from './CircledFontAwesomeIcon';
+import Container from './Container';
+import styles from './Footer.module.scss';
 
 const Footer = ({ ...props }) => (
   <footer {...props}>
-    <FontAwesomeIcon icon={faFacebookF} transform="shrink-8" mask={faCircle} />
-    <FontAwesomeIcon icon={faInstagram} transform="shrink-8" mask={faCircle} />
-    <FontAwesomeIcon icon={faYoutube} transform="shrink-8" mask={faCircle} />
-    <FontAwesomeIcon icon={faEnvelope} transform="shrink-8" mask={faCircle} />
+    <Container>
+      <div className={styles.iconsContainer}>
+        <a href="#todo">
+          <CircledFontAwesomeIcon icon={faFacebookF} className={styles.icon} />
+        </a>
+        <a href="#todo">
+          <CircledFontAwesomeIcon icon={faInstagram} className={styles.icon} />
+        </a>
+        <a href="#todo">
+          <CircledFontAwesomeIcon icon={faYoutube} className={styles.icon} />
+        </a>
+        <a href="#todo">
+          <CircledFontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+        </a>
+      </div>
+    </Container>
   </footer>
 );
+
+Footer.propTypes = {
+  className: PropTypes.string,
+};
+
+Footer.defaultProps = {
+  className: '',
+};
 
 export default Footer;
