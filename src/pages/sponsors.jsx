138bd31251b062be5efdcb8745a1e2424204a473
Kristóf Poduszló <kripod@protonmail.com>
2018-06-25 22:13:01 +0200

    Fix build issue

--- src/pages/sponsors.jsx
+++ src/pages/sponsors.jsx
@@ -1,64 +1,64 @@
-import { graphql, StaticQuery } from 'gatsby';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 import React from 'react';
 import Card from '../components/Card';
 import Container from '../components/Container';
 import Layout from '../components/Layout';
 import styles from './sponsors.module.scss';
 
-const SponsorsPage = () => (
+const SponsorsPage = ({ data }) => (
   <Layout>
-    <StaticQuery
-      query={graphql`
-        query SponsorsPageQuery {
-          allSponsorsYaml {
-            edges {
-              node {
-                category
-                organizations {
-                  name
-                  description
-                  website
-                  logo {
-                    image
-                    height
-                  }
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={staticData =>
-        staticData.allSponsorsYaml.edges.map(({ node }) => (
-          <Container key={node.category}>
-            <h1>{node.category}</h1>
+    {data.allSponsorsYaml.edges.map(({ node }) => (
+      <Container key={node.category}>
+        <h1>{node.category}</h1>
 
-            {node.organizations.map(organization => (
-              <a href={organization.website} className={styles.sponsorLink}>
-                <Card imageSrc={organization.logo.image}>
-                  <Container fluid className={styles.sponsorLogoContainer}>
-                    <img
-                      src={organization.logo.image}
-                      alt=""
-                      className={styles.sponsorLogoImage}
-                      style={{ height: organization.logo.height }}
-                    />
-                  </Container>
+        {node.organizations.map(organization => (
+          <a href={organization.website} className={styles.sponsorLink}>
+            <Card imageSrc={organization.logo.image}>
+              <Container fluid className={styles.sponsorLogoContainer}>
+                <img
+                  src={organization.logo.image}
+                  alt=""
+                  className={styles.sponsorLogoImage}
+                  style={{ height: organization.logo.height }}
+                />
+              </Container>
 
-                  <Container fluid>
-                    <h2 className={styles.sponsorName}>{organization.name}</h2>
-                    {organization.description && (
-                      <p>{organization.description}</p>
-                    )}
-                  </Container>
-                </Card>
-              </a>
-            ))}
-          </Container>
-        ))
-      }
-    />
+              <Container fluid>
+                <h2 className={styles.sponsorName}>{organization.name}</h2>
+                {organization.description && <p>{organization.description}</p>}
+              </Container>
+            </Card>
+          </a>
+        ))}
+      </Container>
+    ))}
   </Layout>
 );
 
+SponsorsPage.propTypes = {
+  data: PropTypes.shape().isRequired,
+};
+
 export default SponsorsPage;
+
+export const query = graphql`
+  query SponsorsPageQuery {
+    allSponsorsYaml {
+      edges {
+        node {
+          category
+          organizations {
+            name
+            description
+            website
+            logo {
+              image
+              height
+            }
+          }
+        }
+      }
+    }
+  }
+`;
