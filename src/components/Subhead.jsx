bd92f0735463356d2312e3ba7be39707f16d6f02
Kristóf Poduszló <kripod@protonmail.com>
2018-08-08 16:13:51 +0200

    Adjust style of subheadings

--- src/components/Subhead.jsx
+++ src/components/Subhead.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Subhead } from 'rebass';
 
-export default props => <Subhead fontFamily="display" {...props} />;
+export default props => <Subhead fontFamily="display" my={3} {...props} />;
