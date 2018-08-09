c7deb136fd7801768f9a07ecd8839b076445b21b
Kristóf Poduszló <kripod@protonmail.com>
2018-08-09 17:20:31 +0200

    Add padding between news items, limit size of each item

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,13 @@
 import { graphql, Link as GatsbyLink, StaticQuery } from 'gatsby';
 import React from 'react';
-import { BackgroundImage, BlockLink, Container } from 'rebass';
+import {
+  BackgroundImage,
+  BlockLink,
+  Box,
+  Container,
+  Flex,
+  Measure,
+} from 'rebass';
 import Card from '../components/Card';
 import CardContent from '../components/CardContent';
 import Heading from '../components/Heading';
@@ -40,29 +47,39 @@ const IndexPage = () => (
             }
           }
         `}
-        render={data =>
-          data.allMarkdownRemark.edges.map(({ node }) => (
-            <BlockLink key={node.id} is={GatsbyLink} to={node.fields.slug}>
-              <Card is="article">
-                <BackgroundImage ratio={9 / 16} src={node.frontmatter.image} />
+        render={data => (
+          <Flex flexWrap="wrap" justifyContent="center" m={-3}>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <Box key={node.id} is={Measure} width={[1, 1, 1 / 2]} p={3}>
+                <BlockLink is={GatsbyLink} to={node.fields.slug}>
+                  <Card is="article">
+                    <BackgroundImage
+                      ratio={9 / 16}
+                      src={node.frontmatter.image}
+                    />
 
-                <CardContent>
-                  <Subhead>{node.frontmatter.title}</Subhead>
+                    <CardContent>
+                      <Subhead>{node.frontmatter.title}</Subhead>
 
-                  <Paragraph>
-                    {node.frontmatter.abstract || node.excerpt}
-                  </Paragraph>
+                      <Paragraph>
+                        {node.frontmatter.abstract || node.excerpt}
+                      </Paragraph>
 
-                  <Paragraph textAlign="right" css={{ fontStyle: 'italic' }}>
-                    <time dateTime={node.frontmatter.date}>
-                      {node.frontmatter.dateString}
-                    </time>
-                  </Paragraph>
-                </CardContent>
-              </Card>
-            </BlockLink>
-          ))
-        }
+                      <Paragraph
+                        textAlign="right"
+                        css={{ fontStyle: 'italic' }}
+                      >
+                        <time dateTime={node.frontmatter.date}>
+                          {node.frontmatter.dateString}
+                        </time>
+                      </Paragraph>
+                    </CardContent>
+                  </Card>
+                </BlockLink>
+              </Box>
+            ))}
+          </Flex>
+        )}
       />
     </Container>
   </Layout>
