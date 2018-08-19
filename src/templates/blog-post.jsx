c9a24fe02ce197c16747d2cb2f32d4f131894399
Kristóf Poduszló <kripod@protonmail.com>
2018-08-19 14:55:29 +0200

    Display blog post title in the page header

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,6 +1,7 @@
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { BackgroundImage, Container, Image, Link } from 'rebass';
 import RehypeReact from 'rehype-react';
 import Blockquote from '../components/Blockquote';
@@ -28,8 +29,14 @@ const BlogPostTemplate = ({ data }) => {
 
   return (
     <Container>
+      <Helmet>
+        <title>{post.frontmatter.title}</title>
+      </Helmet>
+
       <Heading>{post.frontmatter.title}</Heading>
-      <BackgroundImage src={post.frontmatter.image} ratio={9 / 21} mb={3} />
+      {post.frontmatter.image != null && (
+        <BackgroundImage src={post.frontmatter.image} ratio={9 / 21} mb={3} />
+      )}
 
       {renderAst(post.htmlAst)}
     </Container>
