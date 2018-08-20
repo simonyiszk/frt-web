b8113432fbd3fe6e37bad97913a579b53200b3a5
Kristóf Poduszló <kripod@protonmail.com>
2018-08-21 01:13:00 +0200

    Add social media icons  to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Box, Container } from 'rebass';
+import { FacebookF, Instagram, Youtube } from 'styled-icons/fa-brands';
+import { Envelope } from 'styled-icons/fa-solid';
+import IconCircle from './IconCircle';
 
 const Footer = ({ children, ...props }) => (
-  <Box is="footer" py={4} color="white" bg="black" {...props}>
-    <Container>{children}</Container>
+  <Box is="footer" py={3} color="white" bg="black" {...props}>
+    <Container>
+      <Box fontSize={3}>
+        <IconCircle is={FacebookF} title="Facebook" />
+        <IconCircle is={Instagram} title="Instagram" />
+        <IconCircle is={Youtube} title="YouTube" />
+        <IconCircle is={Envelope} title="E-mail" />
+      </Box>
+    </Container>
   </Box>
 );
 
---
+++ src/components/IconCircle.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Box } from 'rebass';
+
+const IconCircle = ({ is: Icon, title, ...props }) => (
+  <Box
+    {...props}
+    css={{
+      display: 'inline-block',
+      width: '2em',
+      padding: '0.5em',
+      borderRadius: '50%',
+      lineHeight: 0,
+    }}
+  >
+    <Icon size="1em" title={title} />
+  </Box>
+);
+
+IconCircle.propTypes = {
+  is: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  color: PropTypes.string,
+  bg: PropTypes.string,
+};
+
+IconCircle.defaultProps = {
+  title: null,
+  color: 'black',
+  bg: 'white',
+};
+
+export default IconCircle;
